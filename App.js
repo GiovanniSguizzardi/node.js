b3a9333e6998e8e984602d2248cd968c2ba23797
Gibas <125572342+GiovanniSguizzardi@users.noreply.github.com>
2023-08-22 09:45:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TesteState from "./componentes/TesteState";
+import './App.css'
+
+function App() {
+  return(
+    <>
+    <h1>Conteúdo do App</h1>
+    <TesteState />
+    </>
+  );
+}
+
+export default App;
